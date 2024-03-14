b48a344d28c737d55a29787178dbdb924006a1b3
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2024-03-14 19:07:03 -0300

    env

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -5,9 +5,9 @@ import path from "path";
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "dncmrwppr",
-  api_key: "756877721848358",
-  api_secret: "avafJRMiK6u_WO89kYKuvd_FQeo",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export async function GET() {
